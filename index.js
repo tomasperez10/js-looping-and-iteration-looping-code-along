bfc96cf14009527902ed1df1c662377838b2590b
Tomas Perez <tomasitopg6@gmail.com>
2021-06-01 07:19:10 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names, event){
+  let birthday = [];
+  event = "surprise";
+  for (let i = 0; i < names.length; i++){
+    birthday[i] = 'Thank you, '+ names[i] +', for the wonderful ' +event+' gift!';
+  }
+  return birthday;
+}
+
+function countDown (number) {
+  while (number >= 0) {
+    console.log(number--);
+  }
+}
